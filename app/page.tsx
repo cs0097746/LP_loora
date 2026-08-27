23be49756824bbe1b1ecbd75c2e515f47bb3f427
Christian Schneider <65926491+cs0097746@users.noreply.github.com>
2026-08-27 08:50:05 -0300

    feat: switch landing page to V3 cinematic hero

--- app/page.tsx
+++ app/page.tsx
@@ -2,19 +2,19 @@ import { ClosingCTA } from '@/components/ClosingCTA';
 import { FAQ } from '@/components/FAQ';
 import { Footer } from '@/components/Footer';
 import { Header } from '@/components/Header';
-import { Hero } from '@/components/Hero';
 import { HowItWorks } from '@/components/HowItWorks';
 import { PainSequence } from '@/components/PainSequence';
 import { ProductShowcase } from '@/components/ProductShowcase';
 import { Security } from '@/components/Security';
 import { TrustStrip } from '@/components/TrustStrip';
+import { CinematicHero } from '@/components/v3/CinematicHero';
 
 export default function HomePage() {
   return (
     <>
       <Header />
       <main>
-        <Hero />
+        <CinematicHero />
         <TrustStrip />
         <PainSequence />
         <ProductShowcase />
