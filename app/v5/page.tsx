065e31ccb1d337e318253b9c781e49826dcef3cc
Christian Schneider <65926491+cs0097746@users.noreply.github.com>
2026-08-30 22:18:53 -0300

    feat: wire V5 first-scroll narrative

--- app/v5/page.tsx
+++ app/v5/page.tsx
@@ -1,6 +1,8 @@
 import styles from './v5.module.css';
 import { V5Header } from '@/components/v5/V5Header';
 import { V5Hero } from '@/components/v5/V5Hero';
+import { InterSessionPressure } from '@/components/v5/InterSessionPressure';
+import { InboundFlow } from '@/components/v5/InboundFlow';
 
 export default function V5Page() {
   return (
@@ -8,6 +10,8 @@ export default function V5Page() {
       <V5Header />
       <main>
         <V5Hero />
+        <InterSessionPressure />
+        <InboundFlow />
       </main>
     </div>
   );
